97e41f69790d7cf1aa87c67fadc1524c525521f5
Thiago Murilo Diniz <thiagodiniz.info@gmail.com>
2024-09-15 21:33:14 -0400

    first commit

---
+++ terraform/variables.tf
@@ -0,0 +1,21 @@
+variable "region" {}
+
+variable "cluster_name" {}
+
+variable "service_name" {}
+
+variable "service_port" {}
+
+variable "service_cpu" {}
+
+variable "service_memory" {}
+
+variable "ssm_vpc_id" {}
+
+variable "ssm_listener" {}
+
+variable "ssm_private_subnet_1" {}
+
+variable "ssm_private_subnet_2" {}
+
+variable "ssm_private_subnet_3" {}
