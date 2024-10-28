02af899a7ebf69a8f940e71ee7b61b4655adc7ae
Thiago Murilo Diniz <thiagodiniz.info@gmail.com>
2024-10-27 22:26:10 -0400

    adds efs volume

---
+++ terraform/efs.tf
@@ -0,0 +1,48 @@
+resource "aws_efs_file_system" "main" {
+  creation_token   = "${var.service_name}-efs"
+  performance_mode = "generalPurpose"
+}
+
+resource "aws_security_group" "efs" {
+  name   = "${var.service_name}-efs"
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
+
+  ingress {
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_efs_mount_target" "mount_1a" {
+  file_system_id = aws_efs_file_system.main.id
+  subnet_id      = data.aws_ssm_parameter.private_subnet_1.value
+  security_groups = [
+    aws_security_group.efs.id
+  ]
+}
+
+resource "aws_efs_mount_target" "mount_1b" {
+  file_system_id = aws_efs_file_system.main.id
+  subnet_id      = data.aws_ssm_parameter.private_subnet_2.value
+  security_groups = [
+    aws_security_group.efs.id
+  ]
+}
+
+resource "aws_efs_mount_target" "mount_1c" {
+  file_system_id = aws_efs_file_system.main.id
+  subnet_id      = data.aws_ssm_parameter.private_subnet_3.value
+  security_groups = [
+    aws_security_group.efs.id
+  ]
+}
